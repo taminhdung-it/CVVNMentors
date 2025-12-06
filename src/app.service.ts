d0ecf7506e66d12628b2d060348064636658aee9
taminhdung-it <taminhdung.it@gmail.com>
2025-12-07 00:27:45 +0700

    kết nối fireauth

--- src/app.service.ts
+++ src/app.service.ts
@@ -31,16 +31,46 @@ export class AppService implements OnModuleInit {
 
     // Ghi đè hoàn toàn
     await docRef.set(data);
-
-    return { path: 'QuanLyCV/information', data };
   }
+
+  private async login_website() {
+    const api_key = this.config.get("database.firebase_api_key")
+    const url = `https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${api_key}`;
+    const res = await fetch(url, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email: "admin@gmail.com", password: "123456789", returnSecureToken: true }),
+    });
+    const data = await res.json();
+
+    if (!res.ok) {
+      console.error('❌Lỗi kết nối fireauth. Báo lỗi: Không trùng khớp dữ liệu');
+      throw new Error()
+    }
+
+    const idToken: string = data.idToken;
+    if (!idToken) {
+      console.error('❌Lỗi kết nối fireauth. Báo lỗi: Không tìm thấy token');
+      throw new Error()
+    }
+
+    const decoded = await this.firebaseApp
+      .auth()
+      .verifyIdToken(idToken)
+      .catch((err) => {
+        console.error('❌Lỗi kết nối fireauth. Báo lỗi: Không tìm thấy token không tồn tại');
+        throw new Error()
+      });
+  };
 
   async onModuleInit() {
     try {
-      const check_connect_firestorage = await this.write_information_website();
+      await this.login_website()
+      console.log('🔥Đã kết nối fireauth thành công');
+      await this.write_information_website();
       console.log('🔥Đã kết nối firestore thành công');
     } catch (e) {
-      console.error('🔥Kết nối firebase thất bại. Báo lỗi', e);
+      console.error('🔥Kết nối firebase thất bại. Báo lỗi:', e);
       process.exit(1);
     }
   }
