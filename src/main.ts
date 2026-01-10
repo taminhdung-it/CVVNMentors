f327305a1e97da895bcfd185f96ca040ecbca094
taminhdung-it <taminhdung.it@gmail.com>
2026-01-11 01:01:45 +0700

    sửa cors

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,7 @@ async function bootstrap() {
       'http://localhost:4200',
       'http://localhost:9999',
       'https://webquanlycv.onrender.com',
-      'webquanlycv.pages.dev'
+      'https://webquanlycv.pages.dev/'
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization','refreshtoken'],
